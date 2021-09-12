5ca85f5489508bd8c482145bf23a41cc44ac2ae4
kdchurch1994 <kdchurch@bsu.edu>
2021-09-12 16:49:04 -0400

    added a function create a new Object Store any time an upgrade is made to the database

--- public/js/idb.js
+++ public/js/idb.js
@@ -2,3 +2,11 @@ let db; //Holds the db connection
 
 //establishes a connection to an IndexedDB database called 'budget-tracker' and sets the version to version 1
 const request = indexedDB.open('budget-tracker', 1);
+
+request.onupgradeneeded = function (event) {
+
+    const db = event.target.result;
+
+    db.createObjectStore('new_input', { autoIncrement: true});
+};
+

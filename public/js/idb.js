84904df3665e799d4d325df97f9b9650126abcd9
kdchurch1994 <kdchurch@bsu.edu>
2021-09-12 17:23:50 -0400

    added comments to upgrade function, added a request.onsuccess function that references an updateBudget function. Still working on the updateBudget function

--- public/js/idb.js
+++ public/js/idb.js
@@ -3,10 +3,12 @@ let db; //Holds the db connection
 //establishes a connection to an IndexedDB database called 'budget-tracker' and sets the version to version 1
 const request = indexedDB.open('budget-tracker', 1);
 
+//this event will only occur when the version of the db changes
 request.onupgradeneeded = function (event) {
 
-    const db = event.target.result;
+    const db = event.target.result; //saves a reference to the database
 
-    db.createObjectStore('new_input', { autoIncrement: true});
+    // creates an object store (similar to a sql table) called `new_budgetInput`, and sets it to have an auto incrementing primary key
+    db.createObjectStore('new_budgetInput', { autoIncrement: true});
 };
 

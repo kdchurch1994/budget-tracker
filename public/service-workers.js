6ef5658fad2ce6d29fe6e35184a8f57c56c1a258
kdchurch1994 <kdchurch@bsu.edu>
2021-09-12 21:16:36 -0400

    fixed issue with service-workers.js file

--- public/service-workers.js
+++ public/service-workers.js
@@ -38,7 +38,7 @@ self.addEventListener('install', function (e) {
     e.waitUntil(
         caches.open(CACHE_NAME).then(function (cache) {
             console.log('Installing cache : ' + CACHE_NAME)
-            return cache.addAll(FILES_TO_CACHE)
+            return cache.addAll(FILES_TO_CACHE);
         })
     )
 })
